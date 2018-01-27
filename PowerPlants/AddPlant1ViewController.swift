effc27026a782786e72e80f5d34f0c22317ccadd
Alyssa Schilke <rosebud1900@gmail.com>
2018-01-27 10:32:34 -0500

    AddPlant

---
+++ PowerPlants/AddPlant1ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  AddPlant1ViewController.swift
+//  PowerPlants
+//
+//  Created by Alyssa Schilke on 1/27/18.
+//  Copyright © 2018 Alyssa Schilke. All rights reserved.
+//
+
+import UIKit
+
+class AddPlant1ViewController: UIViewController {
+
+    @IBOutlet weak var step1Label: UILabel!
+    
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var NextButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    // MARK: - Actions
+    
+    @IBAction func PressNext(_ sender: UIButton) {
+        let plantName = nameField.text
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PowerPlants/AddPlant3ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  AddPlant3ViewController.swift
+//  PowerPlants
+//
+//  Created by Alyssa Schilke on 1/27/18.
+//  Copyright © 2018 Alyssa Schilke. All rights reserved.
+//
+
+import UIKit
+
+class AddPlant3ViewController: UIViewController {
+
+    @IBOutlet weak var nameTitleLabel: UILabel!
+    @IBOutlet weak var nameEntryLabel: UILabel!
+    @IBOutlet weak var kindTitleLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
