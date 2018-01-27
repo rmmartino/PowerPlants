d4047fd95d2f8b0ac64c6ecb3f0d4cd74769d078
Rebecca <rmmartino@loyola.edu>
2018-01-27 15:30:00 -0500

    merging bean

---
+++ Pods/Target Support Files/Pods-PowerPlantsTests/Pods-PowerPlantsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PowerPlantsTests : NSObject
+@end
+@implementation PodsDummy_Pods_PowerPlantsTests
+@end
---
+++ Pods/Target Support Files/Pods-PowerPlantsUITests/Pods-PowerPlantsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PowerPlantsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_PowerPlantsUITests
+@end
